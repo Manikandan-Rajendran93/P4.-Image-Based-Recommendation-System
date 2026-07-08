e7e9e9abf0c897a465c7b086b2bd98db6c82efd4
Manikandan <mani.r27031993@gmail.com>
2026-07-08 17:55:08 +0800

    second commit

--- image_recommendations.py
+++ image_recommendations.py
@@ -1,23 +1,26 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
 from torchvision import transforms
 from PIL import Image
-import torch
 import faiss
 import pickle
 import numpy as np
-import pandas as pd
 import streamlit as st
 
 st.set_page_config(
     page_title = "Image based recommendation",
     page_icon = "🖼️",
     layout = "wide"
-)
+    )
+
 def local_css(file_name):
     try:
         with open(file_name) as f:
             st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     except FileNotFoundError:
         pass
+
 local_css("style.css")
 
 st.markdown('<p class="main-header">🖼️ Image Based Recommendation System</p>', unsafe_allow_html=True)
@@ -30,7 +33,8 @@ def stored_resources():
     index = faiss.read_index("abo_product_vectors.index")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    resnet_model = torch.load("resnet50_feature_extractor.pt", weights_only = False)
+    resnet_model = models.resnet50(weights = models.ResNet50_Weights.DEFAULT)
+    resnet_model.fc = nn.Identity()
     resnet_model = resnet_model.to(device)
     resnet_model.eval()
 
@@ -66,18 +70,19 @@ with st.expander("🗂️ Project Highlights", expanded = False):
 
 st.markdown("<br>", unsafe_allow_html = True)
 
-tab_visual, tab_metric = st.tabs(["💻 VISUAL RECOMMENDATION WINDOW", "📈 EVALUATION METRIC WINDOW"])
+tab_visual, tab_metrics = st.tabs(["💻 VISUAL RECOMMENDATION WINDOW", "📈 EVALUATION METRICS WINDOW"])
 
 with tab_visual:
     st.markdown("<br>", unsafe_allow_html = True)
     
     Product_catalogue, Customer_search, Recommendations = st.columns(3, border = True)
-    with Product_catalogue:
 
+    
+    with Product_catalogue:
         st.markdown('<p class="custom-subheader">Product Catalogue</p>', unsafe_allow_html=True)
         st.markdown("<br>", unsafe_allow_html = True)
         query_path = []
-        for i in range(0, 9950,  199):
+        for i in range(0, 9000,  180):
             query_path.append(products_path_lookup[i])
 
         if "product_selected" not in st.session_state:
@@ -95,16 +100,16 @@ with tab_visual:
                     st.session_state["product_selected"] = path
                     st.rerun()
 
-    with Customer_search:
 
+    with Customer_search:
         user_input = st.session_state["product_selected"]
 
         st.markdown('<p class="custom-subheader">Searched Product</p>', unsafe_allow_html=True)
         st.markdown("<br>", unsafe_allow_html = True)
         st.image(user_input)
 
+    
     with Recommendations:
-
         query_image = Image.open(user_input).convert("RGB")
         
         query_embedding = resnet_transform(query_image)
@@ -121,6 +126,7 @@ with tab_visual:
         distance, indices = index.search(vector_embedding, k = 7)
 
         all_recommended_path = []
+
         for idx in indices[0]:
             recommended_path = products_path_lookup[idx]
             if recommended_path == user_input:
@@ -132,68 +138,76 @@ with tab_visual:
 
         for score, path in zip(distance[0][:len(all_recommended_path)], all_recommended_path):
             st.write(f"Similarity Score: {score:.2%}")
-            st.image(path)
+            st.image(path)            
 
-with tab_metric:
+with tab_metrics:
+    st.markdown('<p class="custom-subheader">Evaluation Metrics</p>', unsafe_allow_html=True)
+    st.markdown("<br>", unsafe_allow_html = True)
+            
+    k = 5
+    sample_queries = 200
+    similarity_thrushold = 0.75
+    
+    all_vectors = []
 
-    evaluation_metrics, = st.columns(1)
-    with evaluation_metrics:
+    for i in range(index.ntotal):
+        catalogue_voctors = all_vectors.append(index.reconstruct(i))
+    catalogue_matrix = np.vstack(all_vectors).astype("float32")
+
+    visual_precisions = []
+    all_cosine_scores = []
+    all_recommended_indices = set()
+
+    np.random.seed(42)
+    query_indices = np.random.choice(index.ntotal, size = sample_queries, replace = False)
+
+    for query_idx in query_indices:
+        query_vector = catalogue_matrix[query_idx].reshape(1, -1)
+
+        distances, indices = index.search(query_vector, k + 1)
+
+        recommended_scores = distances[0][1:]
+        recommended_indices = indices[0][1:]
+
+        visual_relevant_found = np.sum(recommended_scores >= similarity_thrushold)
+        visual_precision_at_k = visual_relevant_found / k
+        visual_precisions.append(visual_precision_at_k)
+
+        all_cosine_scores.extend(recommended_scores)
+        all_recommended_indices.update(recommended_indices)
+
+    avg_visual_precision = np.mean(visual_precisions)
+    avg_cosine_similarity = np.mean(all_cosine_scores)
+
+    unique_items_recommended = len(all_recommended_indices)
+    total_catalogue_size = index.ntotal
+    catalogue_coverage = unique_items_recommended / total_catalogue_size
         
-        k = 5
-        sample_queries = 200
-        similarity_thrushold = 0.75
         
-        all_vectors = []
-        for i in range(index.ntotal):
-            catalogue_voctors = all_vectors.append(index.reconstruct(i))
-        catalogue_matrix = np.vstack(all_vectors).astype("float32")
+    kpi_columns = st.columns(2)
+    kpi_columns[0].metric(
+        label = "Average Visual Precision@K", 
+        value = f'{avg_visual_precision:.2%}'
+        )
+    kpi_columns[1].metric(
+        label = "Average Cosine Similarity@K", 
+        value = f'{avg_cosine_similarity:.2%}'
+        )
 
-        np.random.seed(42)
-        query_indices = np.random.choice(index.ntotal, size = sample_queries, replace = False)
+    st.markdown("<br>", unsafe_allow_html = True)
+    st.markdown("<br>", unsafe_allow_html = True)
+    st.markdown(
+        '<p class="custom-text">ℹ️ Catalogue Coverage and Unique Recommendations'
+        '(For Given K = 5 and Sample Queries = 200 out of 10k)</p>', 
+        unsafe_allow_html=True
+        )
 
-        visual_precisions = []
-        all_cosine_scores = []
-        all_recommended_indices = set()
-
-
-        for query_idx in query_indices:
-            query_vector = catalogue_matrix[query_idx].reshape(1, -1)
-
-            distances, indices = index.search(query_vector, k + 1)
-
-            recommended_scores = distances[0][1:]
-            recommended_indices = indices[0][1:]
-
-            visual_relevant_found = np.sum(recommended_scores >= similarity_thrushold)
-            visual_precision_at_k = visual_relevant_found / k
-            visual_precisions.append(visual_precision_at_k)
-
-            all_cosine_scores.extend(recommended_scores)
-            all_recommended_indices.update(recommended_indices)
-
-        avg_visual_precision = np.mean(visual_precisions)
-        avg_cosine_similarity = np.mean(all_cosine_scores)
-
-        unique_items_recommended = len(all_recommended_indices)
-        total_catalogue_size = index.ntotal
-        catalogue_coverage = unique_items_recommended / total_catalogue_size
-
-
-        st.markdown('<p class="custom-subheader">Evaluation Metrics</p>', unsafe_allow_html=True)
-        st.markdown("<br>", unsafe_allow_html = True)
-        
-        kpi_columns = st.columns(2)
-        kpi_columns[0].metric(
-            label = "Average Visual Precision@K", 
-            value = f'{avg_visual_precision:.2%}')
-        kpi_columns[1].metric(
-            label = "Average Cosine Similarity@K", 
-            value = f'{avg_cosine_similarity:.2%}')
-
-        kpi_columns = st.columns(2)
-        kpi_columns[0].metric(
-            label = "Catalogue Coverage by Model", 
-            value = f'{catalogue_coverage:.2%}')
-        kpi_columns[1].metric(
-            label = "Unique Items Utilized by Engine out of 10000 Products ", 
-            value = f'{unique_items_recommended} Nos.')
+    kpi_columns = st.columns(2)
+    kpi_columns[0].metric(
+        label = "Catalogue Coverage by Model", 
+        value = f'{catalogue_coverage:.2%}'
+        )
+    kpi_columns[1].metric(
+        label = "Unique Items Utilized by Engine out of '10K' Products", 
+        value = f'{unique_items_recommended} Nos.'
+        )
